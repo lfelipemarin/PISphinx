0e87162d6c3ff1e0393d3d1821f3a4b4d580fcbd
Felipe Marin <lfelipe.marin@gmail.com>
2014-11-04 23:28:52 -0500

    Reconociendo frases de un modelo en español. Cambio de librerias a relative path.

--- src/co/edu/udea/sphinxjava/SphinxJava.java
+++ src/co/edu/udea/sphinxjava/SphinxJava.java
@@ -23,26 +23,40 @@ public class SphinxJava {
         Configuration configuration = new Configuration();
 
 // Set path to acoustic model.
-        configuration.setAcousticModelPath("resource:/WSJ_8gau_13dCep_16k_40mel_130Hz_6800Hz");
+//        configuration.setAcousticModelPath("resource:/WSJ_8gau_13dCep_16k_40mel_130Hz_6800Hz");
+        configuration.setAcousticModelPath("voxforge-es-0.1.1/model_parameters/voxforge_es_sphinx.cd_cont_1500");
 // Set path to dictionary.
-        configuration.setDictionaryPath("resource:/WSJ_8gau_13dCep_16k_40mel_130Hz_6800Hz/dict/cmudict.0.6d");
+//        configuration.setDictionaryPath("resource:/WSJ_8gau_13dCep_16k_40mel_130Hz_6800Hz/dict/cmudict.0.6d");
+        configuration.setDictionaryPath("voxforge-es-0.1.1/etc/voxforge_es_sphinx.dic");
 // Set language model.
-        configuration.setLanguageModelPath("models/language/en-us.lm.dmp");
+//        configuration.setLanguageModelPath("models/language/en-us.lm.dmp");
+        configuration.setLanguageModelPath("voxforge-es-0.1.1/etc/voxforge_es_sphinx.transcription.test.lm");
         System.out.println("esta agregando la config al recognizer");
         LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
-        System.out.println("config agregada, diga algo en ingles");
 // Start recognition process pruning previously cached data.
+//        recognizer.startRecognition(true);
+//        SpeechResult result = recognizer.getResult();
+//        System.out.println(result.getHypothesis());
+//        int i = 0;
+//        while ((result = recognizer.getResult()) != null) {
+//            i++;
+//            System.out.println("resultado " + i);
+//            System.out.println(result.getHypothesis());
+//        }
         recognizer.startRecognition(true);
-        SpeechResult result = recognizer.getResult();
-        System.out.println(result.getHypothesis());
-        int i = 0;
-        while ((result = recognizer.getResult()) != null) {
-            i++;
-            System.out.println("resultado " + i);
-            System.out.println(result.getHypothesis());
+        System.out.println("config agregada, diga algo en español");
+        while (true) {
+            String utterance = recognizer.getResult().getHypothesis();
+            if (utterance.equals("personas")) {
+                break;
+            } else {
+                System.out.println(utterance);
+            }
         }
-// Pause recognition process. It can be resumed then with startRecognition(false).
         recognizer.stopRecognition();
+        System.out.println("Reconocedor detenido");
+// Pause recognition process. It can be resumed then with startRecognition(false).
+//        recognizer.stopRecognition();
 
     }
 
